418a40f5afc6ecb16bb54291de152f1176d39c84
alice-maria <mriaalicematos@gmail.com>
2025-10-19 23:12:42 -0300

    ajuste ptbr

--- dose-em-dia-frontend/src/plugins/vuetify.js
+++ dose-em-dia-frontend/src/plugins/vuetify.js
@@ -5,6 +5,7 @@ import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import '@mdi/font/css/materialdesignicons.css';
 import { VDateInput } from 'vuetify/labs/VDateInput'
+import { pt } from 'vuetify/locale' 
 
 export default createVuetify({
   components: {
@@ -14,5 +15,10 @@ export default createVuetify({
   directives,
   icons: {
     defaultSet: 'mdi',
+  },
+   locale: {
+    locale: 'pt',       
+    fallback: 'pt',     
+    messages: { pt },   
   },
 })
