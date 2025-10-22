7b508b40200e19b46ba5919c6e5cad58a69721f2
alice-maria <mriaalicematos@gmail.com>
2025-10-22 19:06:55 -0300

    Adicionado frases para focar mais claro o objetivo dos cards das vacinas

--- dose-em-dia-frontend/src/views/HomeView.vue
+++ dose-em-dia-frontend/src/views/HomeView.vue
@@ -81,9 +81,12 @@
                 <span class="text-h6 fw-bold">{{ vacina.nome }}</span>
               </v-card-title>
               <v-card-text class="pt-1">
-                <p class="mb-1 text-body-1">Aplicada em: {{ formatarData(vacina.dataAplicacao) }}</p>
-                <p class="mb-1 text-body-1">Lote: {{ vacina.lote }}</p>
-                <p class="mb-0 text-body-1">Status: {{ mapearStatus(vacina.status) }}</p>
+                <p class="mb-1 text-body-1">Data de aplicação da dose: {{ formatarData(vacina.dataAplicacao) }}</p>
+                <p class="mb-1 text-body-1">Lote da dose: {{ vacina.lote }}</p>
+                <p class="mb-1 text-body-1">Status da dose: {{ mapearStatus(vacina.status) }}</p>
+                <p class="mt-2 text-body-2">
+                  {{ mensagemStatus(vacina.statusLabel) }}
+                </p>
               </v-card-text>
             </v-card>
           </div>
@@ -206,6 +209,19 @@ export default {
 
     navegar(to) {
       this.$router.push(to);
+    },
+
+    mensagemStatus(statusLabel) {
+      switch (statusLabel) {
+        case STATUS_LABELS.APLICADA:
+          return 'Doses em dia';
+        case STATUS_LABELS.A_VENCER:
+          return 'Essa dose está prestes a vencer';
+        case STATUS_LABELS.VENCIDA:
+          return 'Essa dose passou da data estimada';
+        default:
+          return '';
+      }
     },
   },
 
